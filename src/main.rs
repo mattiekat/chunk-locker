e50c352e2a9e1f00590b1758e8261c36164f9fcf
Emily Hudson <emily33901@gmail.com>
2022-11-07 11:41:08 +0000

    Finish multipart upload

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,56 @@
-mod memory;
-mod db;
-mod store;
+use eyre::Result;
+use std::ops::Deref;
+
+use crate::{
+    memory::{MemoryManager, BUFFER_SIZE},
+    store::{S3Store, Store},
+};
+
 mod chunker;
-mod hasher;
 mod compressor;
+mod config;
+mod db;
 mod encryptor;
+mod hasher;
+mod memory;
 mod signer;
+mod store;
 
 type Hash = u128;
 
-fn main() {
+async fn example_s3store() {
+    let store = S3Store::new().await;
+
+    let manager = MemoryManager::new();
+    let mut handle = manager.alloc().await;
+
+    // Write some garbage into the handle
+    let text = b"Hello chunk-locker!";
+    let text_len = text.len();
+
+    for i in 0..(BUFFER_SIZE.checked_div(text_len).unwrap()) {
+        handle.len += text_len;
+        let bytes = &mut *handle;
+        let range = bytes.len() - text_len..bytes.len();
+        bytes[range].copy_from_slice(text);
+    }
+
+    println!("Uploading to s3");
+    let hash = Hash::default();
+    store.put(hash, handle).await.unwrap();
+
+    println!("Downloading from s3");
+    // try get it back down
+    let handle = store.get(hash).await.unwrap();
+    let bytes = &*handle;
+    println!("{}", String::from_utf8(bytes[..100].to_vec()).unwrap())
+}
+
+#[tokio::main]
+async fn main() {
     // TODO: CLI
     // TODO: TUI
     println!("Hello, world!");
+
+    // example_s3store()
 }
--- src/store.rs
+++ src/store.rs
@@ -1,6 +1,7 @@
 use std::ops::Deref;
 use std::path::PathBuf;
 
+use crate::config::Config;
 use crate::memory::BUFFER_SIZE;
 use crate::memory::{MemoryHandle, MemoryManager};
 use crate::Hash;
@@ -8,17 +9,18 @@ use async_trait::async_trait;
 use aws_sdk_s3 as s3;
 use bytes::Bytes;
 use eyre::Result;
-use s3::model::CompletedPart;
+use s3::model::{CompletedMultipartUpload, CompletedPart};
 use s3::output::CreateMultipartUploadOutput;
 use s3::types::ByteStream;
+use s3::Region;
 
 #[async_trait]
-trait StoreInit {
+pub trait StoreInit {
     async fn lock() -> eyre::Result<Box<dyn Store>>;
 }
 
 #[async_trait]
-trait Store {
+pub trait Store {
     async fn put(&self, hash: Hash, mem: MemoryHandle) -> Result<()>;
     async fn get(&self, hash: Hash) -> Result<MemoryHandle>;
 
@@ -26,7 +28,7 @@ trait Store {
     async fn put_db(&self) -> Result<()>;
 }
 
-struct FSStore {
+pub struct FSStore {
     root: PathBuf,
 }
 
@@ -50,7 +52,9 @@ impl Store for FSStore {
         let bytes = tokio::fs::read(path).await?;
         let mut memory = MemoryManager::new().alloc().await;
 
+        memory.len = bytes.len();
         assert!(bytes.len() < BUFFER_SIZE);
+
         for (i, byte) in bytes.into_iter().enumerate() {
             memory[i] = byte;
         }
@@ -67,18 +71,41 @@ impl Store for FSStore {
     }
 }
 
-struct S3Store {
+pub struct S3Store {
     client: s3::Client,
     base_url: String,
     bucket_name: String,
+    upload_chunk_size: usize,
 }
 
 impl S3Store {
-    pub(crate) fn new() -> Self {
+    pub(crate) async fn new() -> Self {
+        let config = Config::load();
+
+        let creds = s3::Credentials::new(
+            &config.store.s3_key_id,
+            &config.store.s3_application_key,
+            None,
+            None,
+            "chunk-locker",
+        );
+        let endpoint = s3::Endpoint::immutable(config.store.s3_endpoint.parse().unwrap());
+        let region = config.store.s3_region.clone();
+
+        let client = s3::Client::new(
+            &aws_config::from_env()
+                .credentials_provider(creds)
+                .endpoint_resolver(endpoint)
+                .region(Region::new(region))
+                .load()
+                .await,
+        );
+
         Self {
-            client: todo!(),
-            base_url: todo!(),
-            bucket_name: todo!(),
+            client,
+            base_url: config.store.s3_download_url.clone(),
+            bucket_name: config.store.s3_bucket.clone(),
+            upload_chunk_size: config.store.s3_upload_chunk_size,
         }
     }
 }
@@ -102,25 +129,32 @@ impl Store for S3Store {
         let mut upload_parts: Vec<CompletedPart> = Vec::new();
 
         let file_size = mem.len() as usize;
-        const CHUNK_SIZE: usize = 5 * 1024 * 1024;
+        let chunk_size = self.upload_chunk_size;
 
-        let mut chunk_count = (file_size / CHUNK_SIZE) + 1;
-        let mut size_of_last_chunk = file_size % CHUNK_SIZE;
+        let mut chunk_count = (file_size / chunk_size) + 1;
+        let mut size_of_last_chunk = file_size % chunk_size;
         if size_of_last_chunk == 0 {
-            size_of_last_chunk = CHUNK_SIZE;
+            size_of_last_chunk = chunk_size;
             chunk_count -= 1;
         }
 
+        println!("Uploading {chunk_count} chunks....");
+
         let bytes = mem.deref();
+        let total_len = mem.len();
 
         for chunk_index in 0..chunk_count {
-            let this_chunk = if chunk_count - 1 == chunk_index {
+            let this_chunk_size = if chunk_count - 1 == chunk_index {
                 size_of_last_chunk
             } else {
-                CHUNK_SIZE
+                chunk_size
             };
 
-            let bytes = &bytes[chunk_index * CHUNK_SIZE..this_chunk];
+            // TODO(emily): Figure out how best to not copy this memory around
+            // MemoryHandle probably wants to point to a bytes::Bytes structure
+            let bytes = bytes
+                [chunk_index * chunk_size..(chunk_index * chunk_size) + this_chunk_size]
+                .to_vec();
             let stream = ByteStream::from(Bytes::from(bytes));
 
             // Chunk index needs to start at 0, but part numbers start at 1.
@@ -135,18 +169,42 @@ impl Store for S3Store {
                 .part_number(part_number)
                 .send()
                 .await?;
+
             upload_parts.push(
                 CompletedPart::builder()
                     .e_tag(upload_part_res.e_tag.unwrap_or_default())
                     .part_number(part_number)
                     .build(),
             );
+
+            println!(
+                "Uploaded {chunk_index} of {chunk_count} ({} of {})",
+                (chunk_index + 1) * chunk_size,
+                total_len
+            );
         }
+
+        let completed_multipart_upload = CompletedMultipartUpload::builder()
+            .set_parts(Some(upload_parts))
+            .build();
+
+        let _complete_multipart_upload_res = self
+            .client
+            .complete_multipart_upload()
+            .bucket(&self.bucket_name)
+            .key(&key)
+            .multipart_upload(completed_multipart_upload)
+            .upload_id(upload_id)
+            .send()
+            .await
+            .unwrap();
+
+        println!("Completed upload");
 
         Ok(())
     }
     async fn get(&self, hash: Hash) -> Result<MemoryHandle> {
-        let bytes = reqwest::get(format!("{}/{}", self.base_url, hash))
+        let bytes = reqwest::get(format!("{}{}", self.base_url, hash))
             .await?
             .bytes()
             .await?;
@@ -154,11 +212,8 @@ impl Store for S3Store {
         let mut handle = MemoryManager::new().alloc().await;
         let len = bytes.len();
         assert!(BUFFER_SIZE > len);
-        for (i, byte) in bytes.into_iter().enumerate() {
-            handle[i] = byte;
-        }
-
         handle.len = len;
+        handle.copy_from_slice(&bytes);
         Ok(handle)
     }
 
