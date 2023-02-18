6ed814575dfefa740b4d7438ab68748544b6b8c3
Mattie Conover <git@mconover.dev>
2023-02-18 10:51:02 -0800

    Copy over base FastCDC impl

--- src/chunker/mod.rs
+++ src/chunker/mod.rs
@@ -2,6 +2,8 @@ use crate::memory::MemoryHandle;
 use tokio::io::AsyncRead;
 use tokio::sync::mpsc;
 
+mod fast_cdc;
+
 trait Chunker {
     fn chunk<R: AsyncRead>(&self, stream: R) -> mpsc::Sender<MemoryHandle>;
 }
